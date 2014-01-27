44cefd40ab2401998d9d7cc24d63d2d81dfc01a5
Don Kirkby <donkirkby@gmail.com>
2014-01-26 16:14:26 -0800

    Add three text fields, and jump to next field on enter key.

--- vograbulary/src/com/github/donkirkby/vograbulary/VograbularyGame.java
+++ vograbulary/src/com/github/donkirkby/vograbulary/VograbularyGame.java
@@ -2,15 +2,22 @@ package com.github.donkirkby.vograbulary;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 public class VograbularyGame implements ApplicationListener {
 	private Stage stage;
+	private TextField text1;
+    private TextField text2;
+    private TextField text3;
 	private TextButton button;
 	
 	@Override
@@ -18,12 +25,28 @@ public class VograbularyGame implements ApplicationListener {
         Skin skin = new Skin(Gdx.files.internal("data/ui/uiskin.json"));
         
         stage = new Stage();
+        text1 = new TextField("", skin);
+        text2 = new TextField("", skin);
+        text3 = new TextField("", skin);
         button = new TextButton("Click Me!", skin);
+        stage.addActor(text1);
+        stage.addActor(text2);
+        stage.addActor(text3);
         stage.addActor(button);
-        button.addListener(new ClickListener() {
+        button.addListener(new ChangeListener() {
+            
             @Override
-            public void clicked(InputEvent event, float x, float y) {
-                button.setText("Clicked!");
+            public void changed(ChangeEvent event, Actor actor) {
+                nextField();
+            }
+        });
+        stage.addListener(new InputListener() {
+            @Override
+            public boolean keyUp(InputEvent event, int keycode) {
+                if (keycode == Input.Keys.ENTER) {
+                    nextField();
+                }
+                return false;
             }
         });
         Gdx.input.setInputProcessor(stage);
@@ -45,9 +68,19 @@ public class VograbularyGame implements ApplicationListener {
 
 	@Override
 	public void resize(int width, int height) {
+	    float fieldHeight = text1.getHeight();
+        text1.setPosition(
+                (width-button.getWidth())/2, 
+                height-fieldHeight);
+        text2.setPosition(
+                (width-button.getWidth())/2, 
+                height-fieldHeight*2);
+        text3.setPosition(
+                (width-button.getWidth())/2, 
+                height-fieldHeight*3);
         button.setPosition(
                 (width-button.getWidth())/2, 
-                (height-button.getHeight())/2);
+                height-fieldHeight*4);
 	}
 
 	@Override
@@ -57,4 +90,16 @@ public class VograbularyGame implements ApplicationListener {
 	@Override
 	public void resume() {
 	}
+
+    private void nextField() {
+        button.setText("Clicked!");
+        TextField nextField = 
+                stage.getKeyboardFocus() == text1
+                ? text2
+                : stage.getKeyboardFocus() == text2
+                ? text3
+                : text1;
+        nextField.setText("");
+        stage.setKeyboardFocus(nextField);
+    }
 }
