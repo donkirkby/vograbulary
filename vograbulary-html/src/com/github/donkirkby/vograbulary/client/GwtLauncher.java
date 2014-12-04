63cb5210cb584104a42976c1625a118b087561b9
Don Kirkby <donkirkby@gmail.com>
2014-12-04 10:55:19 -0800

    Create a GWT project for issue #42.
    Includes "Hello World" sample code as generated.

--- vograbulary-html/src/com/github/donkirkby/vograbulary/client/GwtLauncher.java
+++ vograbulary-html/src/com/github/donkirkby/vograbulary/client/GwtLauncher.java
@@ -5,15 +5,15 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 
-public class GwtLauncher extends GwtApplication {
-	@Override
-	public GwtApplicationConfiguration getConfig () {
-		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(800, 600);
-		return cfg;
-	}
-
-	@Override
-	public ApplicationListener getApplicationListener () {
-		return new VograbularyApp();
-	}
+public class GwtLauncher { //extends GwtApplication {
+//	@Override
+//	public GwtApplicationConfiguration getConfig () {
+//		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(800, 600);
+//		return cfg;
+//	}
+//
+//	@Override
+//	public ApplicationListener getApplicationListener () {
+//		return new VograbularyApp();
+//	}
 }
