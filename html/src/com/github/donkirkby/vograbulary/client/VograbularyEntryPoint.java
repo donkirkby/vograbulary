e2d8ee8b1fc5e5df0b22adbf5eca7f56b4331d95
Don Kirkby <donkirkby@gmail.com>
2014-12-04 16:13:26 -0800

    Rename host page.

--- html/src/com/github/donkirkby/vograbulary/client/VograbularyEntryPoint.java
+++ html/src/com/github/donkirkby/vograbulary/client/VograbularyEntryPoint.java
@@ -41,7 +41,7 @@ public class VograbularyEntryPoint implements EntryPoint {
     public void onModuleLoad() {
         final Button sendButton = new Button("Send");
         final TextBox nameField = new TextBox();
-        nameField.setText("GWT User");
+        nameField.setText("GWT User name");
         final Label errorLabel = new Label();
 
         // We can add style names to widgets
