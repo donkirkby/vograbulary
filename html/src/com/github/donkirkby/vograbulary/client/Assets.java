0143010d0527b22c40c96c8fbc784d37ddd6466e
Don Kirkby <donkirkby@gmail.com>
2015-09-23 21:59:18 -0700

    Close #61 by adding drag and insert images to client bundle.
    Also move the text assets so we don't get two copies in the Android
    package.

--- html/src/com/github/donkirkby/vograbulary/client/Assets.java
+++ html/src/com/github/donkirkby/vograbulary/client/Assets.java
@@ -2,6 +2,7 @@ package com.github.donkirkby.vograbulary.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.resources.client.TextResource;
 
 public interface Assets extends ClientBundle {
@@ -15,4 +16,10 @@ public interface Assets extends ClientBundle {
 
     @Source("com/github/donkirkby/vograbulary/assets/wordlist.txt")
     TextResource wordList();
+    
+    @Source("com/github/donkirkby/vograbulary/images/drag.png")
+    ImageResource drag();
+    
+    @Source("com/github/donkirkby/vograbulary/images/insert.png")
+    ImageResource insert();
 }
